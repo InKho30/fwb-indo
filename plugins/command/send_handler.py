f76ef14a001c4767c76d51880de428d372895650
kaitod-id <108166655+kaitod-id@users.noreply.github.com>
2023-09-09 17:57:14 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -11,23 +11,18 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
     helper = Helper(client, msg)
     user = db.get_data_pelanggan()
     if msg.text or msg.photo or msg.video or msg.voice:
-        menfess = user.menfess
-        all_menfess = user.all_menfess
         coin = user.coin
-        if menfess >= config.batas_kirim and user.status in [
-            'member',
-            'talent',
-        ]:
-            if coin >= config.biaya_kirim:
-                coin = user.coin - config.biaya_kirim
-            else:
-                return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali.serta coin mu kurang untuk mengirim menfess diluar batas harian., kamu dapat mengirim menfess kembali pada hari esok.\n\n waktu reset jam 1 pagi. \n\n\n\n Info: Topup Coin Hanya ke @bobbyngeped', quote=True)
+
+        if coin >= config.biaya_kirim:
+            coin = user.coin - config.biaya_kirim
+        else:
+            return await msg.reply(f'🙅🏻‍♀️ Post gagal terkirim. Koin Anda tidak mencukupi untuk mengirim pesan. Anda dapat membeli koin lebih banyak.', quote=True)
 
         if key == hastag[0]:
             picture = config.pic_girl
         elif key == hastag[1]:
             picture = config.pic_boy
-            
+
         if user.status == 'talent':
             picture = config.pic_talentgirl
         if user.status == 'admin':
@@ -42,23 +37,15 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
         if user.status == 'girlfriend rent':
             picture = config.pic_gfrent
         if user.status == 'moans boy':
-              if key == hastag[1]:
+            if key == hastag[1]:
                 picture = config.pic_moansboy
-              elif key == hastag[0]:
+            elif key == hastag[0]:
                 picture = config.pic_moansgirl
         elif user.status == 'moans girl':
-              if key == hastag[1]:
+            if key == hastag[1]:
                 picture = config.pic_moansboy
-              elif key == hastag[0]:
+            elif key == hastag[0]:
                 picture = config.pic_moansgirl
-
-            
-            
-
-
-
-
-            
 
         link = await get_link()
         caption = msg.text or msg.caption
@@ -66,8 +53,8 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
 
         kirim = await client.send_photo(config.channel_1, picture, caption, caption_entities=entities)
         await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
-        await db.update_menfess(coin, menfess, all_menfess)
-        await msg.reply(f"pesan telah berhasil terkirim. hari ini kamu telah mengirim menfess sebanyak {menfess + 1}/{config.batas_kirim} . kamu dapat mengirim menfess sebanyak {config.batas_kirim} kali dalam sehari\n\nwaktu reset setiap jam 1 pagi\n<a href='{link + str(kirim.id)}'>check pesan kamu</a>. \n\n\n\n Info: Topup Coin Hanya ke @bobbyngeped")
+        await db.update_coin(coin)  # Perbarui jumlah koin di database
+        await msg.reply(f"pesan telah berhasil terkirim. Koin Anda sekarang {coin}.")
     else:
         await msg.reply('media yang didukung photo, video dan voice')
 
@@ -87,23 +74,18 @@ async def send_menfess_handler(client: Client, msg: types.Message):
             if db_user.status in ['member', 'talent']:
                 return await msg.reply('Tidak bisa mengirim voice, karena sedang dinonaktifkan oleh admin', True)
 
-        menfess = db_user.menfess
-        all_menfess = db_user.all_menfess
         coin = db_user.coin
-        if menfess >= config.batas_kirim and db_user.status in [
-            'member',
-            'talent',
-        ]:
-            if coin >= config.biaya_kirim:
-                coin = db_user.coin - config.biaya_kirim
-            else:
-                return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali.serta coin mu kurang untuk mengirim menfess diluar batas harian., kamu dapat mengirim menfess kembali pada hari esok.\n\n waktu reset jam 1 pagi. \n\n\n\n Info: Topup Coin Hanya ke @bobbyngeped', quote=True)
+
+        if coin >= config.biaya_kirim:
+            coin = db_user.coin - config.biaya_kirim
+        else:
+            return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. Koin Anda tidak mencukupi untuk mengirim pesan. Anda dapat membeli koin lebih banyak.', quote=True)
 
         link = await get_link()
         kirim = await client.copy_message(config.channel_1, msg.from_user.id, msg.id)
         await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
-        await db.update_menfess(coin, menfess, all_menfess)
-        await msg.reply(f"pesan telah berhasil terkirim. hari ini kamu telah mengirim menfess sebanyak {menfess + 1}/{config.batas_kirim} . kamu dapat mengirim menfess sebanyak {config.batas_kirim} kali dalam sehari\n\nwaktu reset setiap jam 1 pagi\n<a href='{link + str(kirim.id)}'>check pesan kamu</a>")
+        await db.update_coin(coin)  # Perbarui jumlah koin di database
+        await msg.reply(f"pesan telah berhasil terkirim. Koin Anda sekarang {coin}.")
     else:
         await msg.reply('media yang didukung photo, video dan voice')
 
@@ -180,14 +162,9 @@ async def transfer_coin_handler(client: Client, msg: types.Message):
             if db_user.status in ['member', 'talent']:
                 return await msg.reply('Tidak bisa mengirim voice, karena sedang dinonaktifkan oleh admin', True)
 
-        menfess = db_user.menfess
-        all_menfess = db_user.all_menfess
         coin = db_user.coin
-        if menfess >= config.batas_kirim and db_user.status in [
-            'member',
-            'talent',
-        ]:
-            if coin >= config.biaya_kirim:
-                coin = db_user.coin - config.biaya_kirim
-            else:
-                return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali.serta coin mu kurang untuk mengirim menfess diluar batas harian., kamu dapat mengirim menfess kembali pada hari esok.\n\n waktu reset jam 1 pagi. \n\n\n\n Info: Topup Coin Hanya ke @bobbyngeped', quote=True)
+
+        if coin >= config.biaya_kirim:
+            coin = db_user.coin - config.biaya_kirim
+        else:
+            return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. Koin Anda tidak mencukupi untuk mengirim pesan. Anda dapat membeli koin lebih banyak.', quote=True)
